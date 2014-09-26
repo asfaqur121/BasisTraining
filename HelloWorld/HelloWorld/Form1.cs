276b17c674add24f7f42be2c491f0e98481cc595
asfaqur121 <asfaqur121@gmail.com>
2014-09-26 09:14:04 +0600

    Frist File

---
+++ HelloWorld/HelloWorld/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HelloWorld
+{
+    public partial class FormShow : Form
+    {
+        public FormShow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonShow_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello World");
+        }
+    }
+}
