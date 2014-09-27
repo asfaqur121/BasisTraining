4489075dd708f1df3ad2a66ff9df495234a35c87
asfaqur121 <asfaqur121@gmail.com>
2014-09-27 16:46:37 +0600

    CalculatorApp

---
+++ CalculatorApp/CalculatorApp/CalculatorApp/Calculator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorApp
+{
+    class Calculator
+    {
+        public double firstNumber;
+        public double secondNumber;
+
+        public double Add()
+        {
+            return firstNumber + secondNumber;
+        }
+
+        public double Substract()
+        {
+            return firstNumber - secondNumber;
+        }
+
+        public double Multiply()
+        {
+            return firstNumber*secondNumber;
+        }
+
+        public double Divide()
+        {
+            return firstNumber/secondNumber;
+        }
+    }
+}
