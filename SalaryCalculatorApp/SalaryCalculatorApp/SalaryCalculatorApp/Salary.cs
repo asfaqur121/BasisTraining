3c54de8625b38a12644b56ace6081d0e901e6a68
asfaqur121 <asfaqur121@gmail.com>
2014-09-27 16:06:33 +0600

    Total Salary Calculator

---
+++ SalaryCalculatorApp/SalaryCalculatorApp/SalaryCalculatorApp/Salary.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalaryCalculatorApp
+{
+    class Salary
+    {
+        public string name;
+        public double basicSalary;
+        public double houseRant;
+        public double medicalRant;
+        public double totalSalary;
+
+
+        public double GetTotal()
+        {
+            double totalSalary = basicSalary + houseRantCalcualte() + medicalRantCalculate();
+            return totalSalary;
+        }
+
+     
+        private double houseRantCalcualte()
+        {
+           return houseRant = (basicSalary*houseRant)/100;
+        }
+
+        private double medicalRantCalculate()
+        {
+           return medicalRant = (basicSalary*medicalRant)/100;
+        }
+
+    }
+}
---
+++ SalaryCalculatorApp/SalaryCalculatorApp/SalaryCalculatorApp/SalaryUI.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SalaryCalculatorApp
+{
+    public partial class SalaryUI : Form
+    {
+        Salary aSalary=new Salary();
+        public SalaryUI()
+        {
+            InitializeComponent();
+        }
+
+        private void totalSalaryShowButton_Click(object sender, EventArgs e)
+        {
+            aSalary.name = nameTextBox.Text;
+            aSalary.basicSalary = Convert.ToDouble(amountTextBox.Text);
+            aSalary.houseRant = Convert.ToDouble(houseRantTextBox.Text);
+            aSalary.medicalRant = Convert.ToDouble(medicalRantTextBox.Text);
+
+            double totalSalaryCalculaton=aSalary.GetTotal();
+            MessageBox.Show(totalSalaryCalculaton.ToString());
+        }
+    }
+}
